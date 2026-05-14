64d589bcc9f8db678321e4da5b29e85b4c21d8cd
Gianpietro Battocletti <battocletti.gianpietro@gmail.com>
2026-05-14 16:55:21 +0200

    Continue work on node implementation

--- px4_braid_mpc/braid_mpc.py
+++ px4_braid_mpc/braid_mpc.py
@@ -8,7 +8,7 @@ from pathlib import Path as FilePath
 import numpy as np
 import yaml
 from ament_index_python import get_package_share_directory
-from nav_msgs.msg import Odometry, Path
+from nav_msgs.msg import Path
 from px4_msgs.msg import (
     VehicleAngularVelocity,
     VehicleAttitude,
@@ -25,12 +25,31 @@ from rclpy.qos import (
 )
 from rclpy.subscription import Subscription
 from trajectory_msgs.msg import MultiDOFJointTrajectory
-from visualization_msgs.msg import Marker
 
 import px4_braid_mpc.messages as messages
+from px4_braid_mpc import interpolation
 
 
 class BraidMPC(Node):
+    """
+    ROS2 node that implements the distributed braid MPC algorithm for a team of
+    PX4-based robots in the ATMOS simulator. The node subscribes to state measurements
+    from the PX4-FMU nodes for each robot, and publishes a reference trajectory for each
+    of the robots. The trajectories are then tracked by a PX4-MPC node running for each
+    robot, which then sends the control commands to the PX4-FMU nodes for execution.
+
+    NOTE: the braid MPC algorithm is designed to be implemented with a distributed
+    scheme. For simplicity, in the current implementation all the 3 braid MPC
+    controllers are run within the same node. This allows to reduce the number of topics
+    and the amount of inter-node communication. However, we highlight that the
+    information used by each MCP controller is still local to a specific robot, and the
+    only difference from a truly distributed implementation is a reduced communication
+    overhead, which would in any case be minimal due to the small amound of information
+    shared between the controllers, and the fact that all the controllers are running on
+    the same machine, with the only information published externally being the control
+    signals for the PX4 flight controllers.
+    """
+
     def __init__(self):
         super().__init__("braid_mpc")
 
@@ -41,7 +60,8 @@ class BraidMPC(Node):
         # Publishers and listeners
         # TODO: check what topics are actually used and remove the unnecessary ones
         # (also remove the corresponding subscribers below)
-        self.publishers: dict[str, Publisher]
+        self.reference_traj_pub: dict[str, Publisher] = {}
+        self.reference_path_pub: dict[str, Publisher] = {}
         self.sub_status: dict[str, Subscription] = {}  # CHECKME: which are useless?
         self.sub_status_v1: dict[str, Subscription] = {}  # CHECKME: needed?
         self.sub_status_v2: dict[str, Subscription] = {}  # CHECKME: needed?
@@ -56,6 +76,11 @@ class BraidMPC(Node):
         self.position: dict[str, VehicleLocalPosition] = {}
         self.attitude: dict[str, VehicleAttitude] = {}
         self.angular_velocity: dict[str, VehicleAngularVelocity] = {}
+
+        # Braid MPC settings
+        # TODO: consider storing settings in config file
+        self.dt = 0.2
+        self.N = 20
 
         # Load data from yaml file
         pkg_share = FilePath(get_package_share_directory("px4_braid_mpc"))
@@ -81,30 +106,22 @@ class BraidMPC(Node):
         # NOTE: topic names are defined as absolute paths with a leading slash since the
         # robot name is already prepended to the topic name.
         self.subscribers = {}
-        self.publishers = {}
+        self.reference_traj_pub = {}
         for ns in self.namespaces:
             # Create trajectory publisher for MPC reference trajectory
-            self.publishers[ns] = self.create_publisher(
+            self.reference_traj_pub[ns] = self.create_publisher(
                 MultiDOFJointTrajectory,
-                f"/{ns}/px4_mpc/reference_trajectory",
+                f"/{ns}/reference_trajectory",
                 qos_profile_pub,
             )
 
-            # Create publisher for rviz visualization
-            self.predicted_path_pub = self.create_publisher(
+            # Create publishers for rviz visualization
+            # To be displayed in combination with the predicted path published by the
+            # PX4-MPC node, which is published on /{ns}/px4_mpc/predicted_path.
+            self.reference_path_pub[ns] = self.create_publisher(
                 Path,
-                f"/{ns}/px4_mpc/predicted_path",
+                f"/{ns}/reference_path",
                 10,
-            )
-            self.reference_pub = self.create_publisher(
-                Marker,
-                f"/{ns}/px4_mpc/reference",
-                10,
-            )
-            self.odom_pub = self.create_publisher(
-                Odometry,
-                f"/{ns}/odom",
-                qos_profile_pub,
             )
 
             # Create subscribers for state measurements
@@ -160,24 +177,47 @@ class BraidMPC(Node):
                 qos_profile_sub,
             )
 
-    def step(self):
-        # TODO: complete implementation
-        # 1. read current state of each robot (e.g. from /odom topic)
-        # 2. update real trajectories + real winding numbers
-        # 3. share state information between robots (e.g. via shared memory as the three
-        #   robots are running in the same process, i.e. this node)
-        # 4. estimate current progress variable tau for each robot + global one
-        # 5. update weights for MPC cost function
-        # 6. solve MPC optimization problem to compute optimal trajectory for each robot
+    def step_callback(self):
+        """
+        Performs one iteration of the MPC loop, which includes the following steps:
+            1. Read current state of each robot (e.g. from /odom topic)
+            2. Update the traveled trajectories and the executed winding numbers
+            3. Estimate the current progress variable tau for each robot
+            4. Share state information and tau estimates with other robots
+            5. Estimate global progress variable tau from local tau estimates
+            6. Update MPC cost weights based on the current tau values
+            7. Solve the MPC optimization problem to compute the optimal trajectory for
+               each robot based on others' predicted trajectories
+            8. Publish the computed trajectory to the corresponding topic for each robot
+               (execution is taken care of by the PX4-MPC nodes running for each robot)
 
-        # 7. publish trajectory to corresponding topic for each robot
-        #   --> execution is taken care of by the PX4-MPC nodes running for each robot
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        # TODO: complete implementation
         trajectories = [np.zeros((10, 13)) for _ in self.namespaces]  # dummy trajectory
+
+        # 8. publish trajectory to corresponding topic for each robot
+        # 8a. interpolate trajectory to match horizon and time step of PX4-MPC
+        interpolated_trajectories = [
+            interpolation.interpolate_trajectory(
+                trajectory=trajectories[i],
+                timestep=self.dt,
+                target_horizon=10,  # TODO: extract from px4-mpc
+                target_timestep=0.1,  # TODO: extract from px4-mpc
+            )
+            for i, _ in enumerate(self.namespaces)
+        ]
+
+        # 8b. publish trajectory
         messages.publish_trajectories(
-            trajectories=trajectories,
+            trajectories=interpolated_trajectories,
             times=0.1,
             namespaces=self.namespaces,
-            publishers=self.publishers,
+            publishers=self.reference_traj_pub,
         )
 
     def _status_callback(self, ns: str, msg: VehicleStatus) -> None:
